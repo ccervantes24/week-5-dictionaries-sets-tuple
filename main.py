bda779715ea14a10e20ef72190db6ada26d26c47
Christopher Cervantes <113720575+ccervantes24@users.noreply.github.com>
2022-09-20 19:30:08 +0000

    booleans

--- main.py
+++ main.py
@@ -1,80 +1,80 @@
-##############################################################################################################
-# what are tuples?
-# exactly the same thing as an array only it is immutable, once you define the tuple,
-#you cannot change it or modify it
-#you define a tuple with () instead of [] like you would in lists
-#once you define it, you cannot change it
-#example
-coordinates = (4, 5)
-# get the first element in the above tuple
-# print(coordinates[1])
-# lets make this a multidimensional array
-coordindates2 = [(4, 5), (6, 7), (80, 34)]
-#get the second element of the second item in coordinates2
+# ##############################################################################################################
+# # what are tuples?
+# # exactly the same thing as an array only it is immutable, once you define the tuple,
+# #you cannot change it or modify it
+# #you define a tuple with () instead of [] like you would in lists
+# #once you define it, you cannot change it
+# #example
+# coordinates = (4, 5)
+# # get the first element in the above tuple
+# # print(coordinates[1])
+# # lets make this a multidimensional array
+# coordindates2 = [(4, 5), (6, 7), (80, 34)]
+# #get the second element of the second item in coordinates2
 
-# we use parentheses not [] in tuples
-my_tuple1 = (1, 2, 3, 4)
-print(my_tuple1[1])
-# get the second item in the tuple1 above
-my_tuple = (1, 2, (10, 20), 4)
-print(my_tuple[1])
-# get the second item in the 3rd item above
-#hint multidimensional array thinking
-print(my_tuple[2][1])
-# place these numbers in separate variables from the tuple below
-t = (1, 2, 3)
-x,y,z = t
-print(x,y,z)
-#get me the length of the above tuple --- there are two ways of doing this... len(), count()...  use count if you want to get how many times an item appears in a tuple
-print(len (my_tuple))
-# Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
+# # we use parentheses not [] in tuples
+# my_tuple1 = (1, 2, 3, 4)
+# print(my_tuple1[1])
+# # get the second item in the tuple1 above
+# my_tuple = (1, 2, (10, 20), 4)
+# print(my_tuple[1])
+# # get the second item in the 3rd item above
+# #hint multidimensional array thinking
+# print(my_tuple[2][1])
+# # place these numbers in separate variables from the tuple below
+# t = (1, 2, 3)
+# x,y,z = t
+# print(x,y,z)
+# #get me the length of the above tuple --- there are two ways of doing this... len(), count()...  use count if you want to get how many times an item appears in a tuple
+# print(len (my_tuple))
+# # Use a tuple method to count the number of times the value 2 appears in the following tuple, and display the result (integer) on the screen:
 
-my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
-print(my_tuple.count(2))
-# Convert the following tuple to a list, and store it in a variable called my_list.
+# my_tuple = (1, 2, 3, 2, 3, 1, 3, 2, 3, 3, 3, 1, 3, 2, 2, 1, 3, 2)
+# print(my_tuple.count(2))
+# # Convert the following tuple to a list, and store it in a variable called my_list.
 
-my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
-my_list = list(my_tuple)
-print(my_list)
+# my_tuple = (1, 2, 3, 2, 3, 1, 3, 2)
+# my_list = list(my_tuple)
+# print(my_list)
       
-#Extract the elements of the following tuple into four variables: a, b, c, d
+# #Extract the elements of the following tuple into four variables: a, b, c, d
 
-my_tuple = (1, 2, 3, 4)
+# my_tuple = (1, 2, 3, 4)
 
-a,b,c,d = my_tuple
-print(a,b,c,d)
-################################################sets######################################################sets are similiar to tuple, the only difference is that they will not contain or print out any repeated elements
+# a,b,c,d = my_tuple
+# print(a,b,c,d)
+# ################################################sets######################################################sets are similiar to tuple, the only difference is that they will not contain or print out any repeated elements
 
-my_firstSet = set((1,2,2,2,3,4,5))
-print(my_firstSet)
-print(2 in my_firstSet)
+# my_firstSet = set((1,2,2,2,3,4,5))
+# print(my_firstSet)
+# print(2 in my_firstSet)
 
-# Join the following sets into one, called my_set_3:
+# # Join the following sets into one, called my_set_3:
 
-s1 = {1, 2, "three", "four"}
+# s1 = {1, 2, "three", "four"}
 
-s2 = {"three", 4, 5}
+# s2 = {"three", 4, 5}
 
-s3 = s1.union(s2)
-print(s3)
+# s3 = s1.union(s2)
+# print(s3)
 
-# Remove a random item from the following set, using set methods.
+# # Remove a random item from the following set, using set methods.
 
-raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-raffle.remove("Rachel") 
-print(raffle)
-raffle.remove("Phoebe") 
-print(raffle)
-# Add the name Gunther to the following set, using set methods:
+# raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+# raffle.remove("Rachel") 
+# print(raffle)
+# raffle.remove("Phoebe") 
+# print(raffle)
+# # Add the name Gunther to the following set, using set methods:
 
-raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
-raffle.add("Gunther")
-print(raffle)
+# raffle = {"Rachel", "Monica", "Phoebe", "Joey", "Chandler", "Ross"}
+# raffle.add("Gunther")
+# print(raffle)
 #################################################booleans######################################################
 # booleans
 # a boolean can only have 2 values -- true or False
-# my_bool = 5 > 4
-#print(my_bool)
+my_bool = 5 > 4
+print(my_bool)
 
 # > greater
 # < less
@@ -84,27 +84,32 @@ print(raffle)
 # != different or not equal to
 
 # you can also construct booleans to see if values ar in a variable or not found
-# my_ bool = 5 in my_list
-# my_bool = 5 not in my_list
+my_list = [1,2,3,4,5,6,7]
+my_bool = 5 in my_list
+my_bool = 5 not in my_list #false
 
 # so we can see if we can make logical decisions if something is true or not
-# var1 = True
-# var2 = False
-# print(type(var1))
-# print(var1)
+var1 = True
+var2 = False
+print(type(var1))
+print(var1)
 
-# list = [1,2,3,4,5,6]
-# control = 5 in list
-# print(type(control))
-# print(control)
+list = [1,2,3,4,5,6]
+control = 5 in list
+print(type(control))
+print(control)
 
 # Booleans Practice
 
 # Make a comparison that returns a boolean and store the result (True/False) in a variable called test
-
+test = 3 >= 4
+print(test)
 # Check if 17834/34 is greater than 87*56 and print the boolean result to the screen using print()
-
+t = 17834/34 > 87*56
+print(t)
 # Check if the square root of 25 is equal to 5 and display the result (boolean) on the screen using print
+import math
+print(math.sqrt(25)==5)
 
 
 
